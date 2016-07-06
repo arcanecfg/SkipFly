6f4717f9089b132c16e854bcd2ddf3e36af681d1
arcanecfg <arcanecfg@hotmail.com>
2016-07-07 04:06:14 +0530

    First
    Allows you to bypass AdFly links.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtAdfly = New System.Windows.Forms.TextBox()
+        Me.btnGetLink = New System.Windows.Forms.Button()
+        Me.cbOpen = New System.Windows.Forms.CheckBox()
+        Me.cbClipboard = New System.Windows.Forms.CheckBox()
+        Me.linkYouTube = New System.Windows.Forms.LinkLabel()
+        Me.linkWastedWolf = New System.Windows.Forms.LinkLabel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(19, 13)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(448, 163)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtAdfly
+        '
+        Me.txtAdfly.Location = New System.Drawing.Point(19, 175)
+        Me.txtAdfly.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtAdfly.Name = "txtAdfly"
+        Me.txtAdfly.Size = New System.Drawing.Size(332, 25)
+        Me.txtAdfly.TabIndex = 1
+        Me.txtAdfly.Text = "Paste AdFly Link Here..."
+        '
+        'btnGetLink
+        '
+        Me.btnGetLink.Location = New System.Drawing.Point(357, 172)
+        Me.btnGetLink.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnGetLink.Name = "btnGetLink"
+        Me.btnGetLink.Size = New System.Drawing.Size(110, 30)
+        Me.btnGetLink.TabIndex = 2
+        Me.btnGetLink.Text = "GET LINK"
+        Me.btnGetLink.UseVisualStyleBackColor = True
+        '
+        'cbOpen
+        '
+        Me.cbOpen.AutoSize = True
+        Me.cbOpen.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.cbOpen.Location = New System.Drawing.Point(301, 208)
+        Me.cbOpen.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.cbOpen.Name = "cbOpen"
+        Me.cbOpen.Size = New System.Drawing.Size(166, 21)
+        Me.cbOpen.TabIndex = 3
+        Me.cbOpen.Text = "O P E N   W E B P A G E"
+        Me.cbOpen.UseVisualStyleBackColor = True
+        '
+        'cbClipboard
+        '
+        Me.cbClipboard.AutoSize = True
+        Me.cbClipboard.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.cbClipboard.Location = New System.Drawing.Point(19, 208)
+        Me.cbClipboard.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.cbClipboard.Name = "cbClipboard"
+        Me.cbClipboard.Size = New System.Drawing.Size(214, 21)
+        Me.cbClipboard.TabIndex = 4
+        Me.cbClipboard.Text = "C O P Y   T O   C L I P B O A R D"
+        Me.cbClipboard.UseVisualStyleBackColor = True
+        '
+        'linkYouTube
+        '
+        Me.linkYouTube.AutoSize = True
+        Me.linkYouTube.LinkColor = System.Drawing.Color.Black
+        Me.linkYouTube.Location = New System.Drawing.Point(16, 244)
+        Me.linkYouTube.Name = "linkYouTube"
+        Me.linkYouTube.Size = New System.Drawing.Size(254, 17)
+        Me.linkYouTube.TabIndex = 5
+        Me.linkYouTube.TabStop = True
+        Me.linkYouTube.Text = "Y O U T U B E . C O M / A R C A N E C F G"
+        '
+        'linkWastedWolf
+        '
+        Me.linkWastedWolf.AutoSize = True
+        Me.linkWastedWolf.LinkColor = System.Drawing.Color.Black
+        Me.linkWastedWolf.Location = New System.Drawing.Point(339, 244)
+        Me.linkWastedWolf.Name = "linkWastedWolf"
+        Me.linkWastedWolf.Size = New System.Drawing.Size(128, 17)
+        Me.linkWastedWolf.TabIndex = 6
+        Me.linkWastedWolf.TabStop = True
+        Me.linkWastedWolf.Text = "W A S T E D W O L F"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(55, Byte), Integer), CType(CType(166, Byte), Integer), CType(CType(222, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(487, 270)
+        Me.Controls.Add(Me.linkWastedWolf)
+        Me.Controls.Add(Me.linkYouTube)
+        Me.Controls.Add(Me.cbClipboard)
+        Me.Controls.Add(Me.cbOpen)
+        Me.Controls.Add(Me.btnGetLink)
+        Me.Controls.Add(Me.txtAdfly)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Arcanecfg SkipFly"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents txtAdfly As System.Windows.Forms.TextBox
+    Friend WithEvents btnGetLink As System.Windows.Forms.Button
+    Friend WithEvents cbOpen As System.Windows.Forms.CheckBox
+    Friend WithEvents cbClipboard As System.Windows.Forms.CheckBox
+    Friend WithEvents linkYouTube As System.Windows.Forms.LinkLabel
+    Friend WithEvents linkWastedWolf As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,58 @@
+﻿'Got the idea from an old JS snippet on my computer.
+'Don't know who the author is but credits to him/her.
+'I simply rewrote it in VB.NET.
+' - Arcanecfg (www.YouTube.com/Arcanecfg)
+
+' # SkipFly
+' -- Lets you bypass AdFly links.
+Imports System.Net
+Imports System.Text
+
+Public Class frmMain
+
+    'https://adf.ly/PNDe3
+    Private Sub btnGetLink_Click(sender As Object, e As EventArgs) Handles btnGetLink.Click
+        Try
+            Dim adfClient As New WebClient()
+            Dim rawStr As String = adfClient.DownloadString(txtAdfly.Text)
+            Dim lineStr As String() = rawStr.Split(Environment.NewLine.ToCharArray, System.StringSplitOptions.RemoveEmptyEntries)
+            Dim reqStr As String = String.Empty
+            For i As Integer = 0 To lineStr.Count - 1
+                If lineStr(i).Contains("ysmm") Then
+                    reqStr = lineStr(i)
+                End If
+            Next
+            reqStr = reqStr.Replace(" ", "")
+            reqStr = reqStr.Remove(0, 9).Replace("'", "").Replace(";", "")
+            Dim D As String = String.Empty
+            Dim F As String = String.Empty
+            For x As Integer = 0 To reqStr.Length - 1
+                If x Mod 2 = 0 Then
+                    D += reqStr(x)
+                Else
+                    F = reqStr(x) + F
+                End If
+            Next
+            reqStr = D + F
+            Dim linkFinal As String = System.Text.Encoding.ASCII.GetString(Convert.FromBase64String(reqStr)).Remove(0, 2)
+            txtAdfly.Text = linkFinal
+            If cbClipboard.Checked = True Then
+                Clipboard.SetText(linkFinal)
+            End If
+            If cbOpen.Checked = True Then
+                System.Diagnostics.Process.Start(linkFinal)
+            End If
+        Catch
+            MessageBox.Show("An error occured, make sure the link is right.", "SkipFly Error", MessageBoxButtons.OK)
+        End Try
+    End Sub
+
+    Private Sub linkYouTube_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkYouTube.LinkClicked
+        System.Diagnostics.Process.Start("https://www.youtube.com/user/ArcaneCfg")
+    End Sub
+
+
+    Private Sub linkWastedWolf_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkWastedWolf.LinkClicked
+        System.Diagnostics.Process.Start("http://www.wastedwolf.com/")
+    End Sub
+End Class
